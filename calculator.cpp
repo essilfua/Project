981c38e2e31c85128b0762299c160d765040eab3
essilfua <109676721+essilfua@users.noreply.github.com>
2022-07-20 14:51:54 -0800

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+//declaration of variables//
+double input1= 0.0;
+double input2= 0.0;
+double output= 0.0;
+
+cout<<"Please enter the first number";
+cin>>input1;
+
+cout<<"Please enter the second number";
+cin>>input2;
+
+output= input1-input2;
+cout>>output;
+
+	return 0;
+}
